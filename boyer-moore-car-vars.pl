6c0fd86a32f60d349100975f60b86c67dee3c5ff
Remi Desmartin <rhd2000@hw.ac.uk>
2025-05-13 19:38:36 +0100

    edit problog example

--- boyer-moore-car-vars.pl
+++ boyer-moore-car-vars.pl
@@ -1,14 +1,18 @@
 /* based on the grading example from Luc De Raedt video https://youtu.be/3lnVBqxjC88?si=24pSYL8O4nWb697t&t=879 */
 
-0.4 :: rain(W) :- weather_state(W).
-  0.5 :: poor(R) :- road_state(R).
 
-  weather_state(w1). weather_state(w2).
-  road_state(r1). road_state(r2).
+/* Facts */
+0.4 :: rain(S) :- scenario(S).
+0.5 :: fog(S) :- scenario(S).
 
-  driving_condition(W,R,good) :- not rain(W), not poor(R).
-  0.3::driving_condition(W,R,good); 0.5::driving_condition(W,R,mid); 0.2::driving_condition(W,R,poor) :- not rain(W), poor(R).
-  0.1::driving_condition(W,R,good); 0.6::driving_condition(W,R,mid); 0.3::driving_condition(W,R,poor) :- rain(W), not poor(R).
-  driving_condition(W,R,poor) :- rain(W), poor(R).
+scenario(s1).
+scenario(s2).
 
-  query(driving_condition(w1,r1,good)).
+/* rules */
+  driving_condition(S,good) :- not rain(S), not fog(S).
+  0.3::driving_condition(S,good); 0.5::driving_condition(S,mid); 0.2::driving_condition(S,poor) :- not rain(S), fog(S).
+  0.1::driving_condition(S,good); 0.6::driving_condition(S,mid); 0.3::driving_condition(S,poor) :- rain(S), not fog(S).
+  driving_condition(S,poor) :- rain(S), fog(S).
+
+/* queries */
+  query(driving_condition(s2,good)).
